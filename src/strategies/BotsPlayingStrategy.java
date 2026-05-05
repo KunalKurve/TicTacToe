0c1e432aaef40b4e69ca9adf442251663ad07321
KunalKurve <osheenmanasi@gmail.com>
2026-05-06 01:37:44 +0530

    Used Strategy Design Pattern for EASY, MEDIUM, HARD difficulty level

--- src/strategies/BotsPlayingStrategy.java
+++ src/strategies/BotsPlayingStrategy.java
@@ -5,6 +5,6 @@ import models.Move;
 import models.Player;
 
 public interface BotsPlayingStrategy {
-
+    // Implemented the Strategy Design Pattern
     Move makeMove(Board board, Player player);
 }
